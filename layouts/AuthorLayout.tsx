099d45af92e0f34c32901f5c309f6afea90e366f
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-02-24 19:01:49 +0100

    Add Resume

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -32,12 +32,22 @@ export default function AuthorLayout({ children, content }: Props) {
             )}
             <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            <div className="text-gray-500 dark:text-gray-400">@{company}</div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
               <SocialIcon kind="twitter" href={twitter} />
+            </div>
+            <div className="mt-10">
+              <a
+                style={{ color: '#1B2329', textDecoration: 'none' }}
+                className="rounded-sm bg-primary-400 px-8 py-4 font-medium"
+                target="_blank"
+                href="/static/images/resume.png"
+              >
+                Resume
+              </a>
             </div>
           </div>
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
