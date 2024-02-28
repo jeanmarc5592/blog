6034ae176790aeba789ae63e08712b6adf00c328
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-02-28 13:38:24 +0100

    Remove unused code

--- app/legal-notice/page.tsx
+++ app/legal-notice/page.tsx
@@ -1,13 +1,9 @@
 import { genPageMetadata } from 'app/seo'
-import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import { LegalNotice, allLegalNotices } from 'contentlayer/generated'
+import { allLegalNotices } from 'contentlayer/generated'
 import Legal from '@/components/Legal'
 
 export const metadata = genPageMetadata({ title: 'Legal Notice' })
 
 export default function Page() {
-  const legalNoticeDE = allLegalNotices.find((p) => p.slug === 'de') as LegalNotice
-  const legalNoticeEN = allLegalNotices.find((p) => p.slug === 'en') as LegalNotice
-
   return <Legal legalTexts={allLegalNotices} />
 }
