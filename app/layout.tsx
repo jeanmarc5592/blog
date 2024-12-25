ce395c9efc08ee42b46e625a684aa2f26f2da54d
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-12-25 22:39:45 +0100

    Disable Pliny Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <body className="bg-white text-black antialiased dark:bg-gray-dark dark:text-white">
         <ThemeProviders>
-          <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
+          {/* <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} /> */}
           <PlausibleProvider domain="jean-marc.io">
             <SectionContainer>
               <div className="flex h-screen flex-col justify-between font-sans">
