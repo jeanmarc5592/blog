df95b818c37c1f972c453c0516888efd085b988e
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-12-25 22:33:07 +0100

    Update ContentSecurityPolicy

--- next.config.js
+++ next.config.js
@@ -7,13 +7,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' plausible.io plausible.io/js;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
-  media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
 `
 
 const securityHeaders = [
